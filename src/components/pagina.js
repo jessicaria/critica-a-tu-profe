8c31e2842f4454b20c6f4fcfef7aaedbd3519d24
santiagoiv <santiagoivan46@gmail.com>
2019-09-13 16:23:33 -0600

    css

--- src/components/pagina.js
+++ src/components/pagina.js
@@ -1,28 +1,38 @@
 import React, { Component } from 'react';
 import './paginas.css';
-class HolaMundo extends Component {
+import imagen from './logo.png';
+import imagen2 from './uach.png';
+class Pagina extends Component {
     render() { 
         return (  
             <React.Fragment>
                <div className ="cuerpo">
                    <div className="encabezado">
-                    <div className="encabezado1">
-                    <h1>Quetzal HEADPHONES</h1>
-                    </div>
-                    <div className="encabezado2">
-                    <div className="bloque"><h1>Inicio</h1></div>
-                    <div className="bloque"><h1>Empresa</h1></div>
-                    <div className="bloque"><h1>Cv</h1></div>
-                    <div className="bloque"><h1>Blog</h1></div>
-                    <div className="bloque"><h1>Contacta</h1></div>
-                    </div>
+                        <div className="encabezado1">
+                            <div className="encabezado2"><img src={imagen} alt="" ></img></div>
+                            <div className="encabezado3"><h1>Critica tu profe</h1></div>
+                        </div>
+                        <div className="flexsearch">
+		                                <div className="flexsearch--wrapper">
+			                            <form className="flexsearch--form" action="#" method="post">
+				                            <div className="flexsearch--input-wrapper">
+					                        <input className="flexsearch--input" type="search" placeholder="search">
+				                            </input>
+				                            <input className="flexsearch--submit" type="submit" value="&#10140;"/>
+                                            </div>
+			                            </form>
+		                            </div>
+                                    
+                            </div>
                    </div>
                    <div className="cuer">
-
+                   <div className="bloque">
+                   <img className="img-small" src={imagen2} alt="" ></img>
                    </div>
-                   <div className="base">
-
+                   <div className="bloque2">carreras ofrecidas</div>
                    </div>
+
+
                 </div> 
             </React.Fragment>
             
@@ -31,4 +41,4 @@ class HolaMundo extends Component {
     }
 }
  
-export default HolaMundo;
+export default Pagina;
